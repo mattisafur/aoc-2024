42605fe7d947a71e5e85a58e0eb37307b1849f7b
mattisafur <46404761+mattisafur@users.noreply.github.com>
2024-12-02 00:28:06 +0200

    finished day 1 of AOC

---
+++ day1/part1.py
@@ -0,0 +1,24 @@
+if __name__ == "__main__":
+    list1: list[int] = []
+    list2: list[int] = []
+
+    # read values
+    with open("input.txt", "r") as input_file:
+        for line in input_file.readlines():
+            values: list[str] = line.split()
+            list1.append(int(values[0]))
+            list2.append(int(values[1]))
+
+    # sort lists
+    list1.sort()
+    list2.sort()
+
+    # sum differences between elements (list2 - list1)
+    diff_sum: int = 0
+    if len(list1) == len(list2):
+        for value1, value2 in zip(list1, list2):
+            diff_sum += abs(value2 - value1)
+    else:
+        raise ValueError("list lengths do not match")
+
+    print(diff_sum)
---
+++ day1/part2.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+    list1: list[int] = []
+    list2: list[int] = []
+
+    # read values
+    with open("input.txt", "r") as input_file:
+        for line in input_file.readlines():
+            values: list[str] = line.split()
+            list1.append(int(values[0]))
+            list2.append(int(values[1]))
+
+    similarity_score: int = 0
+    for value in list1:
+        similarity_score += list2.count(value) * value
+
+    print(similarity_score)
